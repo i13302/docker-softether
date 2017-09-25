4ec8f540bd3eb19ca9a5c3210cb8ac9e0232ade3
i13302 <i13302@sr.kagawa-nct.ac.jp>
2017-09-25 23:57:06 +0900

    docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 BNET="miyalab"
+IP="192.168.1.154"
 
-touch /srv/softether/erc/vpnserver/vpn_server.config
-docker run -d \
+touch /srv/softether/etc/vpnserver/vpn_server.config
+touch /srv/softether/vpnserver/security_log
+touch /srv/softether/vpnserver/packet_log
+touch /srv/softether/vpnserver/server_log
+
+docker run --detach \
 	--volume /srv/softether/vpnserver/vpn_server.config:/usr/local/vpnserver/vpn_server.config \
+	--volume /srv/softether/vpnserver/security_log:/var/log/vpnserver/security_log \
+	--volume /srv/softether/vpnserver/packet_log:/var/log/vpnserver/packet_log \
+	--volume /srv/softether/vpnserver/server_log:/var/log/vpnserver/server_log
 	--privileged \
 	--network ${BNET} \
-	--ip ${192.168.1.154} \
+	--ip ${IP} \
 	--name  softether \
 	frosquin/softether
